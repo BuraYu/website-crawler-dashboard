516a5608ea676f1ec3bdf11d75b02b029e624fcf
Sercan Yüksel <search@Sercans-MacBook-Pro.local>
2025-07-14 15:44:48 +0200

    add skeleton components

---
+++ frontend/src/components/Filters.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Filters: React.FC = () => {
+  return (
+    <div>
+      <h1>Filter</h1>
+    </div>
+  );
+};
+
+export default Filters;
---
+++ frontend/src/components/UrllInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UrlInput: React.FC = () =>{
+    return(
+        <div>
+            <h1>URL input</h1>
+        </div>
+    )
+}
+
+export default UrlInput;
